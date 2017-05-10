87e75b2baff66cad5f91060bec1e290e4fe2daf5
Umberto Raimondi <me@uraimo.com>
2017-05-10 20:40:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# DS1307.swift
+
+*A Swift library for the DS1307 64 x 8, Serial, I2C Real-Time Clock*
+
+<p>
+<img src="https://img.shields.io/badge/os-linux-green.svg?style=flat" alt="Linux-only" />
+<a href="https://developer.apple.com/swift"><img src="https://img.shields.io/badge/swift3-compatible-4BC51D.svg?style=flat" alt="Swift 3 compatible" /></a>
+<a href="https://raw.githubusercontent.com/uraimo/DS1307.swift/master/LICENSE"><img src="http://img.shields.io/badge/license-MIT-blue.svg?style=flat" alt="License: MIT" /></a>
+</p>
+ 
+
+# Summary
+
+This library can configure a DS1307 RTC clock and once it's running can retrieve the current date and time.
+
+This component is powered with 5V and requires a 32.768kHz Quartz Crystal oscillator to operate. It exposes an I2C interface that this library uses to interact with it.
+
+![DS1307 diagram](https://github.com/uraimo/DS1307.swift/raw/master/ds1307.png)
+
+## Supported Boards
+
+Every board supported by [SwiftyGPIO](https://github.com/uraimo/SwiftyGPIO): RaspberryPis, BeagleBones, C.H.I.P., etc...
+
+To use this library, you'll need a Linux ARM board with Swift 3.x.
+
+The example below will use a RaspberryPi 2 board but you can easily modify the example to use one the other supported boards, a full working demo projects for the RaspberryPi2 is available in the `Examples` directory.
+
+## Usage
+
+The first thing we need to do is to obtain an instance of `I2CInterface` from SwiftyGPIO and use it to initialize the `DS1307` object:
+
+```swift
+import SwiftyGPIO
+import DS1307
+
+let i2cs = SwiftyGPIO.hardwareI2Cs(for:.RaspberryPi2)!
+let i2c = i2cs[0]
+
+let ds = DS1307(i2c)
+```
+
+Once you have an instance of the DS1307 object, you can set and get the time as individual components (day, month, year[0-99], hours, minutes, seconds) or as a Foundation's Date or all the components in one go:
+
+```swift
+//Properties of DS1307 (all gettable/settable individually):
+//year,month,date,hours,minutes,seconds
+
+ds.setTime(hours: 16, minutes: 25, seconds: 00, date: 10, month: 5, year: 17)
+ds.start()
+
+var (hours, minutes, seconds, date, month, year) = ds.getTime()
+print("\(year)/\(month)/\(date) \(hours):\(minutes):\(seconds)")
+
+sleep(10)
+
+(hours, minutes, seconds, date, month, year) = ds.getTime()
+print("\(year)/\(month)/\(date) \(hours):\(minutes):\(seconds)")
+
+//Also available:
+//
+//ds.setDate(Date())
+//let now = ds.getDate()
+```
+
+The functions `start()` and `stop()` resume or pause the clock.
+
+
+## Installation
+
+Please refer to the [SwiftyGPIO](https://github.com/uraimo/SwiftyGPIO) readme for Swift installation instructions.
+
+Once your board runs Swift, if your version support the Swift Package Manager, you can simply add this library as a dependency of your project and compile with `swift build`:
+
+```swift
+  let package = Package(
+      name: "MyProject",
+      dependencies: [
+        .Package(url: "https://github.com/uraimo/DS1307.swift.git", majorVersion: 1),
+      ]
+  ) 
+```
+
+The directory `Examples` contains sample projects that uses SPM, compile it and run the sample with `./.build/debug/TestRTC`.
+
+If SPM is not supported, you'll need to manually download the library and its dependencies: 
+
+    wget https://raw.githubusercontent.com/uraimo/DS1307.swift/master/Sources/DS1307.swift https://raw.githubusercontent.com/uraimo/SwiftyGPIO/master/Sources/SwiftyGPIO.swift https://raw.githubusercontent.com/uraimo/SwiftyGPIO/master/Sources/Presets.swift https://raw.githubusercontent.com/uraimo/SwiftyGPIO/master/Sources/I2C.swift https://raw.githubusercontent.com/uraimo/SwiftyGPIO/master/Sources/SunXi.swift  
+
+And once all the files have been downloaded, create an additional file that will contain the code of your application (e.g. main.swift). When your code is ready, compile it with:
+
+    swiftc *.swift
+
+The compiler will create a **main** executable.
+
---
+++ Sources/DS1307.swift
@@ -0,0 +1,163 @@
+/*
+   DS1307.swift
+
+   Copyright (c) 2017 Umberto Raimondi
+   Licensed under the MIT license, as follows:
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.)
+*/
+
+import SwiftyGPIO  //Comment this when not using the package manager
+import Foundation  //Needed for getDate/setDate
+
+
+public class DS1307{
+   var i2c: I2CInterface
+   let address = 0x68
+
+   public init(_ i2c: I2CInterface) {
+       self.i2c = i2c
+   }
+
+   var seconds: Int {
+      get{
+         let rv = i2c.readByte(address, command: 0)
+         return Int(rv & 0xF + ((rv & 0x70)>>4) * 10)
+      }
+      set{
+         let enable = UInt8(seconds) & (~0x80)
+         let v = (newValue/10) << 4 + (newValue % 10)
+         i2c.writeByte(address, command: 0, value: enable|UInt8(v))
+      }
+   }
+
+   var minutes: Int {
+      get{
+         let rv = i2c.readByte(address, command: 1)
+         return Int(rv & 0xF + ((rv & 0x70)>>4) * 10)
+      }
+      set{
+         let v = (newValue/10) << 4 + (newValue % 10)
+         i2c.writeByte(address, command: 1, value: UInt8(v))
+      }
+   }
+
+   var hours: Int {
+      get{
+         let rv = i2c.readByte(address, command: 2)
+         if rv & 0x40 > 1 {
+            // 24h
+            return Int(rv & 0xF + ((rv & 0x30)>>4) * 10)
+         }else{
+            // AM/PM
+            return Int(rv & 0xF + ((rv & 0x20 == 0) ? 12 : 0) )
+         }
+      }
+      set{
+         let v = 0x40 + (newValue/10) << 4 + (newValue % 10)
+         i2c.writeByte(address, command: 2, value: UInt8(v))
+      }
+   }
+
+   // Set/get for day of the week not necessary i guess
+
+   var date: Int {
+      get{
+         let rv = i2c.readByte(address, command: 4)
+         return Int(rv & 0xF + ((rv & 0x30)>>4) * 10)
+      }
+      set{
+         let v = (newValue/10) << 4 + (newValue % 10)
+         i2c.writeByte(address, command: 4, value: UInt8(v))
+      }
+   }
+
+   var month: Int {
+      get{
+         let rv = i2c.readByte(address, command: 5)
+         return Int(rv & 0xF + ((rv & 0x10)>>4) * 10)
+      }
+      set{
+         let v = (newValue/10) << 4 + (newValue % 10)
+         i2c.writeByte(address, command: 5, value: UInt8(v))   
+      }
+   }
+
+   var year: Int {
+      get{
+         let rv = i2c.readByte(address, command: 6)
+         return Int(rv & 0xF + ((rv & 0xF0)>>4) * 10)
+      }
+      set{
+         let v = (newValue/10) << 4 + (newValue % 10)
+         i2c.writeByte(address, command: 6, value: UInt8(v))
+      }
+   }
+
+   func start() {
+      let s = UInt8(seconds)
+      seconds = Int(s & (~0x80))
+   }
+
+   func stop() {
+      let s = UInt8(seconds)
+      seconds = Int(s | 0x80)
+   }
+
+   public func getTime() -> (hours: Int, minutes: Int, seconds: Int, date: Int, month: Int, year: Int) {
+      return (self.hours, self.minutes, self.seconds, self.date, self.month, self.year)
+   }
+
+   public func getDate() -> Date {
+      let formatter = DateFormatter()
+      formatter.dateFormat = "yy/MM/dd HH:mm"
+      let (year,month,date,hours,minutes,seconds) = (self.year,self.month,self.date,self.hours,self.minutes,self.seconds)
+      let dateStr = String(format:"%02d/%02d/%02d %02d:/%02d:%02d",
+                           year,month,date,hours,minutes,seconds)
+      return formatter.date(from: dateStr)!
+   }
+
+   public func setTime(hours: Int, minutes: Int, seconds: Int, date: Int, month: Int, year: Int) {
+      self.year = year
+      self.month = month
+      self.date = date
+      self.hours = hours
+      self.minutes = minutes
+      self.seconds = seconds
+   }
+
+   public func setDate(_ date: Date) {
+      let calendar = Calendar.current
+
+      let hours = calendar.component(.hour, from: date)
+      let minutes = calendar.component(.minute, from: date)
+      let seconds = calendar.component(.second, from: date)
+      let year = calendar.component(.year, from: date)
+      let month = calendar.component(.month, from: date)
+      let date = calendar.component(.day, from: date)
+      self.year = year
+      self.month = month
+      self.date = date
+      self.hours = hours
+      self.minutes = minutes
+      self.seconds = seconds
+   }
+
+   // Square wave control register not implemented
+}
