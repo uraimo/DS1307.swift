c170ad9b5f1c7ae2e8edc4fb41cd41841e93dd94
Umberto Raimondi <me@uraimo.com>
2018-06-05 21:34:35 +0200

    SwiftyGPIO to 1.x

--- Examples/Raspi2/Package.swift
+++ Examples/Raspi2/Package.swift
@@ -2,9 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "TestRTC",
-    targets: [],
     dependencies: [
         .Package(url: "https://github.com/uraimo/DS1307.swift.git",
-                 majorVersion: 1)
+                 majorVersion: 2)
     ]
 )
